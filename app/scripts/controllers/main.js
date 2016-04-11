820f661a8267f7e29e32f6495ceaa644698e66a3
Alexander <alexander.kosak@icloud.com>
2016-04-11 07:26:59 +0200

    make it better

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsMytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsMytodoApp
+ */
+angular.module('jsMytodoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
